f676639ddeab0a320adc8d91329b1a22689f3981
oscar <oscaroalesgonzalez@gmail.com>
2023-11-07 17:42:02 +0100

    inicio interfaz pistas

---
+++ app/src/main/java/com/example/proyectopadel/pistas.java
@@ -0,0 +1,14 @@
+package com.example.proyectopadel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pistas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pistas);
+    }
+}
